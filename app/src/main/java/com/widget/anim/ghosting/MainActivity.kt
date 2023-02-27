81cf9acd948800b505998b4036ea6d18aa9181a2
陈文管 <40523194+wenguan0927@users.noreply.github.com>
2023-02-27 11:53:18 +0800

    优化版本Demo实现

--- app/src/main/java/com/widget/anim/ghosting/MainActivity.kt
+++ app/src/main/java/com/widget/anim/ghosting/MainActivity.kt
@@ -4,28 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val mHandler = Handler(Looper.getMainLooper())
-    private var mScore = 0
+    private var mScore = 7
+    private var mScoreAbsoluteSizeSpan: AbsoluteSizeSpan? = null
+    private var mScoreTailAbsoluteSizeSpan: AbsoluteSizeSpan? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mScoreAbsoluteSizeSpan = AbsoluteSizeSpan(
+            resources.getDimensionPixelSize(R.dimen.score_text_size),
+            false
+        )
+        mScoreTailAbsoluteSizeSpan = AbsoluteSizeSpan(
+            resources.getDimensionPixelSize(R.dimen.score_text_size) / 20,
+            false
+        )
 
-        mPlayAnim.setOnClickListener(object: View.OnClickListener{
+        mPlayAnim.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                mScore = 0
+                mScore = 1
                 mHandler.removeCallbacksAndMessages(null)
-                showScore()
+                setScore()
             }
         })
     }
 
-    fun showScore(){
-        mScore +=2
-        mScoreAnimV.showScore(mScore)
+    /* fun showScore(){
+        mScore += 1
+        mScoreAnimTv.showScore(mScore)
         if(mScore < 3){
             mMTrapezoidV.setColor(MultiTrapezoidView.COLOR_LEVEL_GOOD)
         } else if(mScore >= 3 && mScore <6){
@@ -34,10 +48,48 @@ class MainActivity : AppCompatActivity() {
             mMTrapezoidV.setColor(MultiTrapezoidView.COLOR_LEVEL_PERFECT)
         }
         mHandler.postDelayed({
-            if(mScore < 13) {
+            if(mScore < 10) {
                 showScore()
             }
-        }, 1500)
+        }, 2000)
+    } */
+
+    fun setScore() {
+        mScore += 1
+
+        if (mScore % 10 == 7) {
+            //避免右上角被裁剪，使用小尺寸空格填充
+            var str7 = " ${mScore} "
+            var spannable7 = SpannableString(str7)
+            spannable7.setSpan(
+                mScoreTailAbsoluteSizeSpan, 0, 1, Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable7.setSpan(
+                mScoreAbsoluteSizeSpan, 1, str7.length - 1, Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable7.setSpan(
+                mScoreTailAbsoluteSizeSpan,
+                str7.length - 1,
+                str7.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            mGhostingTv.setScore(spannable7)
+        } else {
+            mGhostingTv.setScore(mScore.toString(), mScore % 10 == 0)
+        }
+
+        if (mScore < 3) {
+            mMTrapezoidV.setColor(MultiTrapezoidView.COLOR_LEVEL_GOOD)
+        } else if (mScore >= 3 && mScore < 6) {
+            mMTrapezoidV.setColor(MultiTrapezoidView.COLOR_LEVEL_GREAT)
+        } else if (mScore >= 6) {
+            mMTrapezoidV.setColor(MultiTrapezoidView.COLOR_LEVEL_PERFECT)
+        }
+        mHandler.postDelayed({
+            if (mScore < 12) {
+                setScore()
+            }
+        }, 1000)
     }
 
     override fun onStop() {
